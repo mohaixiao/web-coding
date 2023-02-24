3cc924c351c847547765f9b0be282f2b796482a7
suixin <suixin@zhihu.com>
2023-02-24 15:36:01 +0800

    add

--- 算法/数组/滑动窗口/904. 水果成篮.js
+++ 算法/数组/滑动窗口/904. 水果成篮.js
@@ -5,26 +5,27 @@
  * @return {number}
  */
 var totalFruit = function (fruits) {
-    let i = 0;
-    let n = 0; // 记录第一类水果最开始的位置并且更新给i
-    let maxLen = 0;
-    let arr = [fruits[i]];
-    for (let j = 0; j < fruits.length; j++) {
-        if (!arr.includes(fruits[j])) {
-            // 换个篮子继续装
-            if (arr.length <= 1) {
-                arr[1] = fruits[j];
-            } else {
-                // 2个篮子换果子
-                i = n;
-                arr[0] = fruits[j - 1];
-                arr[1] = fruits[j];
-            }
-        }
-        if (fruits[n] !== fruits[j]) {
-            n = j;
-        }
-        maxLen = Math.max(maxLen, j - i + 1);
+  let i = 0;
+  let n = 0; // 记录一类水果最开始的位置并且更新给i
+  let maxLen = 0;
+  let arr = [fruits[i]];
+  for (let j = 0; j < fruits.length; j++) {
+    // 遇到篮子中没有的新水果
+    if (!arr.includes(fruits[j])) {
+      // 换个篮子继续装
+      if (arr.length <= 1) {
+        arr[1] = fruits[j];
+      } else {
+        // 2个篮子换果子
+        i = n;
+        arr[0] = fruits[j - 1];
+        arr[1] = fruits[j];
+      }
     }
-    return maxLen;
+    if (fruits[n] !== fruits[j]) {
+      n = j;
+    }
+    maxLen = Math.max(maxLen, j - i + 1);
+  }
+  return maxLen;
 };
