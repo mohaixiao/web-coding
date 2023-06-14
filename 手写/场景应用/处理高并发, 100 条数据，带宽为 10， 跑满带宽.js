d69b42009b8f191005e695912fa8b2747deed153
mohaixiao <396367702@qq.com>
2023-06-14 21:27:24 +0800

    update

--- 手写/场景应用/处理高并发, 100 条数据，带宽为 10， 跑满带宽.js
+++ 手写/场景应用/处理高并发, 100 条数据，带宽为 10， 跑满带宽.js
@@ -1,24 +1,38 @@
-function asyncPool(limit, dates, fn) {
-    let count = 0;
-    const exec = [];
-    const values = [];
-    const _asyncPool = () => {
-        if (count === dates.length) {
-            return Promise.resolve();
-        }
-
-        const item = dates[count++];
-        const p = Promise.resolve().then(() => fn(item));
-        values.push(p);
-
-        const _p = p.then(() => exec.splice(exec.indexOf(_p), 1));
-        exec.push(_p);
-
-        let next = Promise.resolve();
-        if (exec.length >= limit) {
-            next = Promise.race(exec);
-        }
-        return next.then(() => _asyncPool());
+function control(stack, max, cb) {
+    let data = [];
+    let len = stack.length;
+    if (!max) {
+        max = len;
     }
-    return _asyncPool().then(() => Promise.all(values));
+    function loop() {
+        if (!stack.length) return cb(data) ;
+        if (stack.length < max) max = stack.length;
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let data = []
+                for (let i = 0; i < max; i++) {
+                    const element = stack[i];
+                    data.push(element(i));
+                }
+                resolve(data);
+            }, 1000)
+        }).then(res => {
+            data.push(...res);
+            stack = stack.slice(max);
+            loop()
+        })
+    }
+    loop()
 }
+
+let stack = [
+    res => 1,
+    res => 2,
+    res => 3,
+    res => 4,
+    res => 5
+]
+
+control(stack, 3, res => {
+    console.log(res);
+})
